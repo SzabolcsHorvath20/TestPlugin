e491886832b327afc7fe675171a49b5a47006306
garru <garruswasab@gmail.com>
2022-01-29 12:05:52 +0100

    Add project files.

---
+++ ClassLibrary1/ValidateEmail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+
+namespace Plugins
+{
+    public class ValidateEmail : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)
+            serviceProvider.GetService(typeof(IPluginExecutionContext));
+            Entity entity = (Entity)context.InputParameters["Target"];
+            string name = (string)entity["lastname"];
+            if ("Test".Equals(name))
+            {
+                entity["cr1f8_valid"] = true;
+            }
+        }
+    }
+}
