0730de8b5aedf1ce5ac57f8b9518c2962667724b
garru <garruswasab@gmail.com>
2022-01-29 15:14:01 +0100

    Revert "Http Post request added, without the bearer token"
    This reverts commit a641fc6af37214bf2cd911d6c477510143c933de.

--- ClassLibrary1/ValidateEmail.cs
+++ ClassLibrary1/ValidateEmail.cs
@@ -1,76 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Query;
-using Newtonsoft.Json;
+
 
 namespace Plugins
 {
-
-
     public class ValidateEmail : IPlugin
     {
-        public class ValidatedEmail
-        {
-            public string Email { get; set; }
-            public bool Valid { get; set; }
-            public string Token { get; set; }
-        }
-
         public void Execute(IServiceProvider serviceProvider)
         {
             IPluginExecutionContext context = (IPluginExecutionContext)
             serviceProvider.GetService(typeof(IPluginExecutionContext));
             Entity entity = (Entity)context.InputParameters["Target"];
-            if (entity.Contains("emailaddress1"))
+            string email = (string)entity["emailaddress1"];
+            bool isEmail = Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+            if (isEmail)
             {
-                string email = (string)entity["emailaddress1"];
-                bool isEmail = Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
-                if (isEmail)
-                {
-                    entity["cr1f8_valid"] = true;
-                    PostRequest(email, true);
-                   
-                }
-            }
-            else
-            {
-                entity["cr1f8_valid"] = false;
-                //PostRequest((string)entity["emailaddress1"], false);
-            }
-        }
-
-        public void PostRequest(string email, bool valid)
-        {
-            var url = "https://crane-hire.azurewebsites.net/api/email_validation?code=Bwp9Qq81v7Q2u2hgFaRqzKuh394wlBoSRFU32Z5UO5v2yd/tVNvhZg==";
-            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpRequest.Method = "POST";
-            httpRequest.Accept = "application/json";
-            httpRequest.Headers["Authorization"] = "Bearer ";
-            httpRequest.ContentType = "application/json";
-
-            var data = new ValidatedEmail()
-            {
-                Email = email,
-                Valid = valid,
-                Token = ""
-
-            };
-            string json = JsonConvert.SerializeObject(data);
-            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
-            {
-                streamWriter.Write(json);
-            }
-            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
+                entity["cr1f8_valid"] = true;
             }
         }
     }
